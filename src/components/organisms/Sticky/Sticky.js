7208ff4d71de4169f1539b724a49d3e65c678233
Johnathan Walker <johnathan.walker@vodafone.com>
2019-09-26 11:36:40 +0100

    Corrected use of window to on componentDidMount

--- src/components/organisms/Sticky/Sticky.js
+++ src/components/organisms/Sticky/Sticky.js
@@ -7,14 +7,17 @@ import onScroll from 'helpers/onScroll';
 
 import If from 'components/utils/If';
 
-export default function Sticky({ whenToShow, children }) {
+// Sticky wrapper, will render childen after user has stopped scroll to defined point
+export default function Sticky({ children, delay, whenToShow }) {
+  // Boolean value to hide/show children
   const [inView, setInView] = useState(false);
 
-  // On start of scroll, should view out of view
+  // On START of scroll, should view out of view
   const start = () => setInView(false);
 
-  const stop = () => {
-    if (whenToShow > document.documentElement.scrollTop) {
+  // On STOP of scroll, setInView depending on where we're scrolled to
+  const stop = (minPx) => {
+    if (minPx > document.documentElement.scrollTop) {
       // Sets to FALSE, if we're within top half of page
       setInView(false);
     } else {
@@ -26,7 +29,12 @@ export default function Sticky({ whenToShow, children }) {
   // Add scroll listener ONCE on initial render
   useEffect(() => {
     // Pass start/stop callbacks to onScroll handler, along with a delay to trigger the "stop" callback
-    const scrollCallBack = window.addEventListener('scroll', () => onScroll(start, stop, 500));
+    const scrollCallBack = window.addEventListener('scroll', () => onScroll(
+      start,
+      // Only has access to window when mounted
+      () => stop(window.innerHeight * whenToShow),
+      delay,
+    ));
 
     return () => {
       // Tidy up after ourselves
@@ -41,6 +49,16 @@ export default function Sticky({ whenToShow, children }) {
       position: fixed;
       width: 100%;
       z-index: 40;
+
+      -webkit-transform: translateY(-100%);
+      transform: translateY(-100%);
+      transition: transform 300ms ease-in-out;
+
+      ${inView && `
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+        transition: transform 300ms ease-in-out;
+      `}
     `}
     >
       <If condition={inView}>
@@ -51,12 +69,15 @@ export default function Sticky({ whenToShow, children }) {
 }
 
 Sticky.propTypes = {
-  // Pixel value when to start showing sticky children
-  whenToShow: PropTypes.number,
   // Sticky children to display
   children: PropTypes.node.isRequired,
+  // Delay "stop" callback on scroll finish
+  delay: PropTypes.number,
+  // When to start to show children, fraction of 100vh (1/2, 1/3, 3/2 etc... )
+  whenToShow: PropTypes.number,
 };
 
 Sticky.defaultProps = {
-  whenToShow: (window.innerHeight / 2),
+  delay: 1000,
+  whenToShow: 1 / 3,
 };
